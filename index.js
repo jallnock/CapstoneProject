450c6b709cb4a2d402e4f0c22274d1ec57a526ff
John Doe <johndoe@email.com>
2025-03-13 20:10:59 -0400

    root index.js set up

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const { PORT = 3000 } = process.env;
+const express = require("express");
+const server = express();
+
+const bodyParser = require("bodyparser");
+server.use(bodyParser.json());
+
+const morgan = require("morgan");
+server.use(morgan("dev"));
+
+server.use((req, res, next) => {
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
+  next();
+});
+
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
+
+const { client } = require("./db");
+client.connect();
+
+const path = require("path");
+server.use(express.static(path.join(__dirname, public)));
+
+server.listen(PORT, () => {
+  console.log("The server is running on port", PORT);
+});
