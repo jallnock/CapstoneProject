22412df7c7e7873b35f15a7d222f4a555cc8caa3
John Doe <johndoe@email.com>
2025-03-14 16:42:33 -0400

    review endpoints created

---
+++ api/reviews.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const reviewsRouter = express.Router();
+const {
+  getAllReviews,
+  getReviewsById,
+  createReview,
+  updateReview,
+  deleteReview,
+} = require("..db");
+
+//GET ALL REVIEWS
+reviewsRouter.get("/", async (req, res, next) => {
+  try {
+    const reviews = await getAllReviews();
+    res.send({ reviews });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET A SINGLE REVIEW BY ID
+reviewsRouter.get("/:reviewID", async (req, res, next) => {
+  try {
+    const { reviewId } = req.params;
+    const review = await getReviewsById(reviewId);
+    if (review) {
+      res.send({ review });
+    } else {
+      next({
+        name: "ReviewNotFoundError",
+        message: "Review not found",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+//POST NEW REVIEW
+reviewsRouter.post("/", async (req, res, next) => {
+  try {
+    const { user_id, restaurant_id, rating, review_description } = req.body;
+    const newReview = await createReview({
+      user_id,
+      restaurant_id,
+      rating,
+      review_description,
+    });
+    res.send({ message: "Review created", newReview });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//PATCH / UPDATE review
+reviewsRouter.patch("/:reviewId", async (req, res, next) => {
+  try {
+    const { reviewId } = req.params;
+    const { rating, review_description } = req.body;
+    const updatedReview = await updateReview(reviewId, {
+      rating,
+      review_description,
+    });
+    res.send({ message: "Review updated", updatedReview });
+  } catch (error) {
+    next(error);
+  }
+});
